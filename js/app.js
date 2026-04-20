c07d983f06c92ecba4fc34e7f2a358db7562365c
Dardan <dardan.has@gmail.com>
2026-04-20 16:59:02 +0200

    Countdown, TODO etc

--- js/app.js
+++ js/app.js
@@ -1,21 +1,33 @@
 // Click a button or object to earn points so that I can increase my score. DONE
 // See my current score during the game so that I know how well I am doing. DONE
+
 // See a countdown timer so that I know how much time is left. setInterval();
 
 // Variables
 let score = 0;
+let timeleft = 60;
 
 //HTML DOM
 const button1 = document.getElementById('button1');
 const ScoreDisplay = document.getElementById('ScoreDisplay');
-
-// UI Functions
+const TimerDisplay = document.getElementById('TimerDisplay');
+// UI Functions & Events
 button1.addEventListener('click', () => {
   increaseScore();
 })
+
+// TODO: Start at button click
+setInterval(countdown, 1000);
+
 
 // Functions
 function increaseScore() {
 score++;
 ScoreDisplay.textContent = score;
+}
+
+function countdown() {
+  timeleft--;
+  TimerDisplay.textContent = timeleft;
+  //TODO: Stop timer at 0 
 }
